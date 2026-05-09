2c9a5477af26b8378b49752d100f870050b3d261
koljaPl <nikolya.plugin@gmail.com>
2026-05-09 21:54:14 +0200

    I didn't plan on it, but I ended up making a Fenwick tree or a binary indexed tree, even though I had intended to make a B-tree

---
+++ algorithms/tree/design/fenwick_tree.py
@@ -0,0 +1,31 @@
+# Time complexity:
+# Building a Fenwick tree:  O(n log n)
+# Optimal construction:     O(n)
+# Updating an element:      O(log n)
+# Prefix sum:               O(log n)
+# Sum over a range:         O(log n)
+#
+# Memory usage:             O(n)
+
+class FenwickTree:
+    def __init__(self, n):
+        self.n = n
+        self.bit = [0] * (n + 1)
+
+    # Add function (also known as update)
+    def add(self, i, value):
+        while i <= self.n:
+            self.bit[i] += value
+            i += i & -i
+
+    def prefix_sum(self, i):
+        prefix_sum = 0
+
+        while i > 0:
+            prefix_sum += self.bit[i]
+            i -= i & -i
+
+        return prefix_sum
+
+    def range_sum(self, start, end):
+        return self.prefix_sum(start) - self.prefix_sum(end - 1)
