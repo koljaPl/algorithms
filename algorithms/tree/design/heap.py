02282e5bf1f731c09cc45d0d51fed937444bd14c
koljaPl <nikolya.plugin@gmail.com>
2026-05-13 17:28:37 +0200

    I’ve implemented the full range of heap algorithms (min-heap and max-heap) and learnt everything I could about them (I’ll need to go back and study them properly later, as my understanding is still rather superficial at the moment).  Well then, I’m off to the shops with Mum now, and afterwards I’ll tackle the red-black tree; I’ll try to get it all done today.

---
+++ algorithms/tree/design/heap.py
@@ -0,0 +1,188 @@
+# Heap - Min Heap and Max Heap
+
+# Min Heap AND Max Heap:
+# Insert:        O(log n)
+# Pop:           O(log n)
+# Top:           O(1)
+# Search:        O(n)
+#
+# Heapify list:  O(n)
+
+def get_parent(i):
+    return (i - 1) // 2
+
+def get_left(i):
+    return 2 * i + 1
+
+def get_right(i):
+    return 2 * i + 2
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = (
+            self.heap[j],
+            self.heap[i]
+        )
+
+    def sift_up(self, i):
+        while i > 0:
+            parent = get_parent(i)
+
+            if self.heap[parent] <= self.heap[i]:
+                break
+
+            self.swap(parent, i)
+            i = parent
+
+    def sift_down(self, i):
+        n = len(self.heap)
+
+        while True:
+            left = get_left(i)
+            right = get_right(i)
+
+            smallest = i
+
+            if left < n and self.heap[left] < self.heap[smallest]:
+                smallest = left
+
+            if right < n and self.heap[right] < self.heap[smallest]:
+                smallest = right
+
+            if smallest == i:
+                break
+
+            self.swap(i, smallest)
+            i = smallest
+
+    def push(self, value):
+        self.heap.append(value)
+        self.sift_up(len(self.heap) - 1)
+
+    def pop(self):
+        if not self.heap:
+            raise IndexError("Heap is empty")
+
+        minimum = self.heap[0]
+
+        last = self.heap.pop()
+
+        if self.heap:
+            self.heap[0] = last
+            self.sift_down(0)
+
+        return minimum
+
+    def heapify(self, arr):
+        self.heap = arr[:]
+
+        for i in range(len(self.heap) // 2 - 1, -1, -1):
+            self.sift_down(i)
+
+    def top(self):
+        if not self.heap:
+            raise IndexError("Heap is empty")
+
+        return self.heap[0]
+
+    def __len__(self):
+        return len(self.heap)
+
+    def __repr__(self):
+        return str(self.heap)
+
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = (
+            self.heap[j],
+            self.heap[i]
+        )
+
+    def sift_up(self, i):
+        while i > 0:
+            parent = get_parent(i)
+
+            if self.heap[parent] >= self.heap[i]:
+                break
+
+            self.swap(parent, i)
+            i = parent
+
+    def sift_down(self, i):
+        n = len(self.heap)
+
+        while True:
+            left = get_left(i)
+            right = get_right(i)
+
+            largest = i
+
+            if left < n and self.heap[left] > self.heap[largest]:
+                largest = left
+
+            if right < n and self.heap[right] > self.heap[largest]:
+                largest = right
+
+            if largest == i:
+                break
+
+            self.swap(i, largest)
+            i = largest
+
+    def push(self, value):
+        self.heap.append(value)
+        self.sift_up(len(self.heap) - 1)
+
+    def pop(self):
+        if not self.heap:
+            raise IndexError("Heap is empty")
+
+        maximum = self.heap[0]
+
+        last = self.heap.pop()
+
+        if self.heap:
+            self.heap[0] = last
+            self.sift_down(0)
+
+        return maximum
+
+    def heapify(self, arr):
+        self.heap = arr[:]
+
+        for i in range(len(self.heap) // 2 - 1, -1, -1):
+            self.sift_down(i)
+
+    def top(self):
+        if not self.heap:
+            raise IndexError("Heap is empty")
+
+        return self.heap[0]
+
+    def __len__(self):
+        return len(self.heap)
+
+    def __repr__(self):
+        return str(self.heap)
+
+# Tss, there is something cool about max heap, there is how we do this in Python:
+
+# import heapq # Just a cool pythonic library with heap implementation
+
+# heap = []
+
+# heapq.heappush(heap, -10)
+# heapq.heappush(heap, -5)
+# heapq.heappush(heap, -20)
+
+# print(-heapq.heappop(heap))
+
+# See? It's just numbers with - and we print it also like - so we get anti-min-heap what is max heap.
