b95261ed324a98caa07f77a8cbe1fea55f43b985
koljaPl <nikolya.plugin@gmail.com>
2026-05-15 21:09:55 +0200

    I've solved a couple of problems from CSES, so here are my commits: Problem - Number Spiral

---
+++ problems/number_spiral/number_spiral.py
@@ -0,0 +1,23 @@
+import sys
+# sys.setrecursionlimit(200000)
+
+input = sys.stdin.readline
+
+def main():
+    t = int(input())
+
+    for _ in range(t):
+        y, x = map(int, input().split())
+
+        if y > x:
+            if y % 2 == 0:
+                print(y ** 2 - x + 1)
+            else:
+                print((y - 1) ** 2 + x)
+        else:
+            if x % 2 == 1:
+                print(x ** 2 - y + 1)
+            else:
+                print((x - 1) ** 2 + y)
+
+main()
