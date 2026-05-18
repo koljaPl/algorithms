8c9000b781d085baa53576f7494e6aa8938b3dca
koljaPl <nikolya.plugin@gmail.com>
2026-05-18 20:30:07 +0200

    Scheduled code refactoring and retraining

--- algorithms/tree/design/segment_tree.py
+++ algorithms/tree/design/segment_tree.py
@@ -9,40 +9,47 @@
 # Memory:   O(4 * n)
 
 class SegmentTree:
-    def __init__(self, arr):
+    def __init__(self, arr, merge, identity):
         self.n = len(arr)
-
         self.arr = arr[:]
-        self.tree = [0] * (4 * self.n)
+        self.tree = [identity] * (4 * self.n)
 
-        self.built(1, 0, self.n - 1)
+        self.merge = merge
+        self.identity = identity
 
-    def built(self, node, left, right):
+        self.build(1, 0, self.n - 1)
+
+    def build(self, node, left, right):
         if left == right:
             self.tree[node] = self.arr[left]
             return
 
         mid = left + (right - left) // 2
-        self.built(node * 2, left, mid)
-        self.built(node * 2 + 1, mid + 1, right)
-        self.tree[node] = self.tree[node * 2] + self.tree[node * 2 + 1]
+
+        self.build(node * 2, left, mid)
+        self.build(node * 2 + 1, mid + 1, right)
+
+        self.tree[node] = self.merge(
+            self.tree[node * 2],
+            self.tree[node * 2 + 1]
+        )
 
     def query(self, start, end):
         return self.helper_query(1, 0, self.n - 1, start, end)
 
     def helper_query(self, node, left, right, start, end):
         if end < left or right < start:
-            return 0
+            return self.identity
 
         if start <= left and right <= end:
             return self.tree[node]
 
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
 
-        s1 = self.helper_query(node * 2, left, mid, start, end)
-        s2 = self.helper_query(node * 2 + 1, mid + 1, right, start, end)
+        left_side = self.helper_query(node * 2, left, mid, start, end)
+        right_side = self.helper_query(node * 2 + 1, mid + 1, right, start, end)
 
-        return s1 + s2
+        return self.merge(left_side, right_side)
 
     def update(self, index, value):
         self.helper_update(1, 0, self.n - 1, index, value)
@@ -52,63 +59,110 @@ class SegmentTree:
             self.tree[node] = value
             return
 
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
+
         if index <= mid:
             self.helper_update(node * 2, left, mid, index, value)
         else:
             self.helper_update(node * 2 + 1, mid + 1, right, index, value)
 
-        self.tree[node] = self.tree[node * 2] + self.tree[node * 2 + 1]
+        self.tree[node] = self.merge(
+            self.tree[node * 2],
+            self.tree[node * 2 + 1]
+        )
 
 
 class IterativeSegmentTree:
-    def __init__(self, arr):
+    def __init__(self, arr, merge, identity):
         self.n = len(arr)
         self.size = 1
         while self.size < self.n:
             self.size *= 2
 
-        self.tree = [0] * (2 * self.size)
+        self.tree = [identity] * (2 * self.size)
+
+        self.merge = merge
+        self.identity = identity
 
         for i in range(self.n):
             self.tree[self.size + i] = arr[i]
 
         for i in range(self.size - 1, 0, -1):
-            self.tree[i] = self.tree[i * 2] + self.tree[i * 2 + 1]
+            self.tree[i] = self.merge(
+                self.tree[i * 2],
+                self.tree[i * 2 + 1]
+            )
 
     def set(self, idx, value):
         pos = self.size + idx
         self.tree[pos] = value
-        pos //= 2
 
+        pos //= 2
         while pos >= 1:
-            self.tree[pos] = self.tree[pos * 2] + self.tree[pos * 2 + 1]
+            self.tree[pos] = self.merge(
+                self.tree[2 * pos],
+                self.tree[2 * pos + 1]
+            )
             pos //= 2
 
     def query(self, left, right):
         left += self.size
-        right += self.size
-        res = 0
+        right += self.size + 1
+
+        res_left = self.identity
+        res_right = self.identity
 
         while left < right:
             if left % 2 == 1:
-                res += self.tree[left]
+                res_left = self.merge(res_left, self.tree[left])
                 left += 1
             if right % 2 == 1:
                 right -= 1
-                res += self.tree[right]
+                res_right = self.merge(self.tree[right], res_right)
+
             left //= 2
             right //= 2
 
-        return res
+        return self.merge(res_left, res_right)
 
 # How to use:
 
-# arr = [5, 2, 7, 3, 6, 1]
-# st = SegmentTree(arr)
-# st = IterativeSegmentTree(arr)
+# SUM tree:
+# st = SegmentTree(
+#     arr,
+#     merge=lambda a, b: a + b,
+#     identity=0
+# )
 
-# print(st.query(1, 3)) # Sum
+# MIN tree:
+# st = SegmentTree(
+#     arr,
+#     merge=min,
+#     identity=10**18
+# )
+
+# MAX tree:
+# st = SegmentTree(
+#     arr,
+#     merge=max,
+#     identity=-10**18
+# )
+
+# XOR tree:
+# st = SegmentTree(
+#     arr,
+#     merge=lambda a, b: a ^ b,
+#     identity=0
+# )
+
+# GCD tree
+# import math
+#
+# st = SegmentTree(
+#     arr,
+#     merge=math.gcd,
+#     identity=0
+# )
 
 # When to use:
 # Он очень часто встречается в задачах типа:
