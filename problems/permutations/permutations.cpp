1a2d2b73c75dffdc4ab220ceb76a55baa8f1cb78
koljaPl <nikolya.plugin@gmail.com>
2026-05-23 18:23:58 +0200

    I've solved a couple of problems from CSES, so here are my commits: Problem - Permutations (C++)

---
+++ problems/permutations/permutations.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n = 1;
+    cin >> n;
+
+    if (n == 1) {
+        cout << 1 << "\n";
+        return 0;
+    } else if (n < 4) {
+        cout << "NO SOLUTION" << "\n";
+        return 0;
+    } else {
+        vector<int> res;
+        
+        for (int i = 2; i < n + 1; i += 2) {
+            res.push_back(i);
+        }
+        
+        for (int i = 1; i < n + 1; i += 2) {
+            res.push_back(i);
+        }
+
+        for (int num : res) {
+            std::cout << num << " "; 
+        }
+        return 0;
+    }
+
+    return 0;
+}
