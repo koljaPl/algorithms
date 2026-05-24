6e5a39be403d7800e18283331fa23891ad66891b
koljaPl <nikolya.plugin@gmail.com>
2026-05-24 14:54:00 +0200

    I've solved a problem from CSES, so here are my commit: Problem - Gray Code (C++)

---
+++ problems/gray_code/gray_code.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n = 0;
+    cin >> n;
+
+    vector<string> gray = {""};
+
+    for (int i = 0; i < n; i++) {
+        vector<string> next;
+        
+        next.reserve(gray.size() * 2);
+
+        for (auto &x : gray)
+            next.push_back("0" + x);
+
+        for (auto it = gray.rbegin(); it != gray.rend(); ++it)
+            next.push_back("1" + *it);
+
+        gray = move(next);
+    }
+
+    for (auto &s : gray)
+        cout << s << "\n";
+
+    return 0;
+}
