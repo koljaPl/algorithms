cf3fbae461dc50d301c4a8a679259204c40ff1de
koljaPl <nikolya.plugin@gmail.com>
2026-06-17 19:11:11 +0200

    I've solved a couple of problems from CSES, so here are my commits: Problem - Traffic Lights

---
+++ problems/CSES/Sorting_and_Searching/traffic_lights/traffic_lights.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x, n;
+    cin >> x >> n;
+
+    set<int> lights;
+    multiset<int> lengths;
+
+    lights.insert(0);
+    lights.insert(x);
+    lengths.insert(x);
+
+    for (int i = 0; i < n; i++) {
+        int p;
+        cin >> p;
+
+        auto it = lights.upper_bound(p);
+
+        int right = *it;
+        int left = *prev(it);
+
+        lengths.erase(lengths.find(right - left));
+
+        lengths.insert(p - left);
+        lengths.insert(right - p);
+
+        lights.insert(p);
+
+        cout << *lengths.rbegin() << ' ';
+    }
+
+    cout << '\n';
+}
