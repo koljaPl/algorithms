716ef37ed90af62f90a1bc11fdc8f3509b379443
koljaPl <nikolya.plugin@gmail.com>
2026-06-19 19:17:36 +0200

    I've solved a couple of problems from CSES, so here are my commits: Problem - Movie Festival (C++)

---
+++ problems/CSES/Sorting_and_Searching/movie_festival/movie_festival.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    vector<pair<int, int>> movies(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> movies[i].first >> movies[i].second;
+    }
+
+    sort(movies.begin(), movies.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second < b.second;
+    });
+
+    int res = 0, last_end = 0;
+    for (const auto& movie : movies) {
+        if (movie.first >= last_end) {
+            res++;
+            last_end = movie.second;
+        }
+    }
+
+    cout << res << "\n";
+
+    return 0;
+}
