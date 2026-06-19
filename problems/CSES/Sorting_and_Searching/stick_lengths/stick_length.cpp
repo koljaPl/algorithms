fab2728d97dcc8dd7b2f1e8d6f0eadb65ccc7c7b
koljaPl <nikolya.plugin@gmail.com>
2026-06-19 19:22:26 +0200

    I've solved a couple of problems from CSES, so here are my commits: Problem - Stick Lengths (C++)

---
+++ problems/CSES/Sorting_and_Searching/stick_lengths/stick_length.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    vector<ll> lengths(n);
+    for (int i = 0; i < n; i++) cin >> lengths[i];
+    sort(lengths.begin(), lengths.end());
+
+    ll res = 0;
+    ll needed = lengths[n / 2];
+
+    for (int i = 0; i < n; i++) {
+        res += abs(lengths[i] - needed);
+    }
+
+    cout << res << "\n";
+
+    return 0;
+}
