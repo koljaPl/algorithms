7164eaa3ccc1d9fc81a979880b078b72f862a158
koljaPl <nikolya.plugin@gmail.com>
2026-06-20 17:20:21 +0200

    I've solved a couple of problems from CSES, so here are my commits: Problem - Missing Coin Sum (C++)

---
+++ problems/CSES/Sorting_and_Searching/missing_coin_sum/missing_coin_sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n;
+    cin >> n;
+    
+    vector<ll> coins(n);
+    for (int i = 0; i < n; ++i) cin >> coins[i];
+
+    sort(coins.begin(), coins.end());
+
+    ll res = 1;
+    for (ll coin : coins) {
+        if (coin > res) break;
+
+        res += coin;
+    }
+
+    cout << res;
+
+    return 0;
+}
