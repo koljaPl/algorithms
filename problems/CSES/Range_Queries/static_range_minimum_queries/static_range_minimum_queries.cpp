4ba6c397d295a7d0ba2dce46a2eac8ef293a7d0c
koljaPl <nikolya.plugin@gmail.com>
2026-09-25 17:05:13 +0200

    I've solved a couple of problems from CSES, so here are my commits: Problem - Static Range Minimum Queries (C++)

---
+++ problems/CSES/Range_Queries/static_range_minimum_queries/static_range_minimum_queries.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+
+template <typename T, typename Merge>
+class IterativeSegmentTree {
+private:
+    int n;
+    int size;
+
+    vector<T> tree;
+
+    Merge merge;
+    T identity;
+
+public:
+    IterativeSegmentTree(
+        const vector<T>& arr,
+        Merge merge,
+        T identity
+    )
+        : n(arr.size()),
+          merge(merge),
+          identity(identity)
+    {
+        size = 1;
+
+        while (size < n) {
+            size *= 2;
+        }
+
+        tree.assign(2 * size, identity);
+
+        // Leaves
+        for (int i = 0; i < n; i++) {
+            tree[size + i] = arr[i];
+        }
+
+        // Build
+        for (int i = size - 1; i >= 1; i--) {
+            tree[i] = merge(
+                tree[i * 2],
+                tree[i * 2 + 1]
+            );
+        }
+    }
+
+    void set(int idx, T value) {
+        int pos = size + idx;
+
+        tree[pos] = value;
+
+        pos /= 2;
+
+        while (pos >= 1) {
+            tree[pos] = merge(
+                tree[pos * 2],
+                tree[pos * 2 + 1]
+            );
+
+            pos /= 2;
+        }
+    }
+
+    T query(int left, int right) {
+        left += size;
+
+        // [left, right] -> [left, right)
+        right += size + 1;
+
+        T res_left = identity;
+        T res_right = identity;
+
+        while (left < right) {
+            if (left % 2 == 1) {
+                res_left = merge(
+                    res_left,
+                    tree[left]
+                );
+
+                left++;
+            }
+
+            if (right % 2 == 1) {
+                right--;
+
+                res_right = merge(
+                    tree[right],
+                    res_right
+                );
+            }
+
+            left /= 2;
+            right /= 2;
+        }
+
+        return merge(res_left, res_right);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    auto merge = [](int a, int b) { return min(a, b); };
+
+    IterativeSegmentTree tree(arr, merge, INT_MAX);
+
+    for (int i = 0; i < q; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << tree.query(a - 1, b - 1) << '\n';
+    }
+
+    return 0;
+}
