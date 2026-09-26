0829173832baf4cd154b23be03e0d0fe05dbe400
koljaPl <nikolya.plugin@gmail.com>
2026-09-26 14:40:54 +0200

    I've solved a couple of problems from CSES, so here are my commits: Problem - Dynamic Range Sum Queries (C++)

---
+++ problems/CSES/Range_Queries/dynamic_range_sum_queries/dynamic_range_sum_queries.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+
+class FenwickTree {
+private:
+    int n;
+    vector<ll> bit;
+
+public:
+    explicit FenwickTree(int size) : n(size), bit(size + 1, 0) {}
+
+    explicit FenwickTree(const vector<int>& values)
+        : n(static_cast<int>(values.size())), bit(n + 1, 0) {
+
+        for (int i = 1; i <= n; i++) {
+            bit[i] += values[i - 1];
+
+            int parent = i + (i & -i);
+
+            if (parent <= n) {
+                bit[parent] += bit[i];
+            }
+        }
+    }
+
+    void add(int i, ll value) {
+        while (i <= n) {
+            bit[i] += value;
+            i += i & -i;
+        }
+    }
+
+    void subtract(int i, ll value) {
+        add(i, -value);
+    }
+
+    ll prefix_sum(int i) {
+        ll res = 0;
+
+        while (i > 0) {
+            res += bit[i];
+            i -= i & -i;
+        }
+
+        return res;
+    }
+
+    ll range_sum(int left, int right) {
+        return prefix_sum(right) - prefix_sum(left - 1);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    FenwickTree bit(n);
+
+    for (int i = 0; i < n; i++) bit.add(i + 1, arr[i]);
+
+    for (int i = 0; i < q; i++) {
+        int type, a, b;
+        cin >> type >> a >> b;
+
+        if (type == 1) {
+            int delta = b - arr[a - 1];
+            arr[a - 1] = b;
+            bit.add(a, delta);
+        } else {
+            cout << bit.range_sum(a, b) << "\n";
+        }
+    }
+
+    return 0;
+}
